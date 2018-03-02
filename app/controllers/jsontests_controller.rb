2d4b011621c4b008ffb3eec0cdac48321373bcb9
Tech-AYY <tech-ayy@Tech-AYYs-MacBook-Air.local>
2018-03-02 10:37:59 +0000

    mvp

--- app/controllers/jsontests_controller.rb
+++ app/controllers/jsontests_controller.rb
@@ -9,62 +9,60 @@ class JsontestsController < ApplicationController
   # GET /jsontests
   # GET /jsontests.json
   def index
-    json = HTTParty.get("https://api.typeform.com/v1/form/WaIffL?key=f486f2db8f1249c077a08b582bc3efe0a2617668").body
-    @jsontests = JSON.parse(json)
-
-    ids = []
-    @trainers = []
-    @questions = @jsontests["questions"]
-    @responses = @jsontests["responses"]
-    @questions.each do |question|
-       ids.push(question["id"])
-    end
-    @responses.each do |response|
-      @trainers.push(response["answers"]["dropdown_oXn12MMLCWad"])
-      @trainers = @trainers.uniq
-    end
-
-    @mappingHash = @responses.map do |response|
-      @trainer = {
-        'Q_Score' => response["answers"]["opinionscale_b2FX1apYy6KY"].to_i,
-        'Q_Organised'=> response["answers"]["list_YMBzp5MSVXw6_choice"],
-        'Q_Industry'=> response["answers"]["list_O30DQPjBp5vo_choice"],
-        'Q_Examples'=> response["answers"]["list_maGlk0GN5zP0_choice"],
-        'Q_Feedback'=> response["answers"]["list_tXrYhpNCS0pt_choice"],
-        'Q_Contact'=> response["answers"]["list_HK4vmpWmUCqV_choice"],
-        'Q_ClearProjects'=> response["answers"]["list_H5HwuUY7llmz_choice"],
-        'Q_TName' => response["answers"]["dropdown_oXn12MMLCWad"],
-        'Q_feedbacktext'=> response["answers"]["textfield_WfUP6q9XNCMX"],
-        'Q_DrawsAttention'=> response["answers"]["list_Da75t6LS98SA_choice"]
-      }
-
-    end
-
-    @trainerNPS = @responses.map do |trainer|
-      @NPS = {
-        'Trainer Name' => trainer["answers"]["dropdown_oXn12MMLCWad"],
-        'NPS' => trainer["answers"]["opinionscale_b2FX1apYy6KY"].to_i
-      }
-    end
-    counter = 0
-    npsTotal = 0
-    @trainerNPS.each do |i|
-      if i['Trainer Name'] == "Joe Childs"
-        counter += 1
-        if i['NPS'] <= 6
-          npsTotal -= 1
-        elsif i['NPS'] <=8
-          npsTotal += 0
-        else
-          npsTotal += 1
+      json = HTTParty.get("https://api.typeform.com/v1/form/WaIffL?key=f486f2db8f1249c077a08b582bc3efe0a2617668").body
+      @jsontests = JSON.parse(json)
+      ids = []
+      @trainers = []
+      @questions = @jsontests["questions"]
+      @responses = @jsontests["responses"]
+      @questions.each do |question|
+         ids.push(question["id"])
+      end
+      @responses.each do |response|
+        if response["answers"]["dropdown_oXn12MMLCWad"]!= ""
+          @trainers.push(response["answers"]["dropdown_oXn12MMLCWad"])
         end
-        @NPSScore = (npsTotal/counter.to_f) * 100
+        @trainers = @trainers.uniq
+      end
+      @mappingHash = @responses.map do |response|
+        @trainer = {
+          'Q_Score' => response["answers"]["opinionscale_b2FX1apYy6KY"].to_i,
+          'Q_Organised'=> response["answers"]["list_YMBzp5MSVXw6_choice"],
+          'Q_Industry'=> response["answers"]["list_O30DQPjBp5vo_choice"],
+          'Q_Examples'=> response["answers"]["list_maGlk0GN5zP0_choice"],
+          'Q_Feedback'=> response["answers"]["list_tXrYhpNCS0pt_choice"],
+          'Q_Contact'=> response["answers"]["list_HK4vmpWmUCqV_choice"],
+          'Q_ClearProjects'=> response["answers"]["list_H5HwuUY7llmz_choice"],
+          'Q_TName' => response["answers"]["dropdown_oXn12MMLCWad"],
+          'Q_feedbacktext'=> response["answers"]["textfield_WfUP6q9XNCMX"],
+          'Q_DrawsAttention'=> response["answers"]["list_Da75t6LS98SA_choice"]
+        }
+      end
+      @trainerNPS = @responses.map do |trainer|
+        @NPS = {
+          'Trainer Name' => trainer["answers"]["dropdown_oXn12MMLCWad"],
+          'NPS' => trainer["answers"]["opinionscale_b2FX1apYy6KY"].to_i
+        }
+      end
+      @NPSScore = []
+      @trainers.each do |trainer_name|
+        counter = 0
+        npsTotal = 0
+        @trainerNPS.each do |i|
+          if i['Trainer Name'] == trainer_name
+            counter += 1
+            if i['NPS'] <= 6
+              npsTotal -= 1
+            elsif i['NPS'] <=8
+              npsTotal += 0
+            else
+              npsTotal += 1
+            end
+          end
+        end
+        @NPSScore.push((npsTotal/counter.to_f)* 100)
       end
     end
-    puts @NPSScore
-    puts counter
-
-  end
 
   # GET /jsontests/1
   # GET /jsontests/1.json
